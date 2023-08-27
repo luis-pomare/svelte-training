05499db5e0393a03c2eef26e4da81f4e2c582b81
Luis Pomare <luispomare92@gmail.com>
2023-08-27 00:25:44 -0500

    Create header section

--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,5 @@
 <script>
-	export let name;
+	import Header from './sections/Header.svelte'
 </script>
 
-<main>
-	<h1>Hello {name.toUpperCase()}!</h1>
-	<img src="/svelteIcon.png" alt="svelteIcon"/>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-
-		img {
-			width: 50vw;
-		}
-	}
-</style>
+<Header />
---
+++ src/sections/Header.svelte
@@ -0,0 +1,33 @@
+<script>
+  let name = 'luis';
+</script>
+
+<header>
+  <h1>Hello {name.charAt(0).toLocaleUpperCase()+name.slice(1)}!</h1>
+  <img src="/svelteIcon.png" alt="Svelte icon">
+</header>
+
+<style>
+	header {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		header {
+			max-width: none;
+		}
+
+		img {
+			width: 50vw;
+		}
+	}
+</style>
