e459aac83a8d018060e4d686257828cdcfe43976
Luis Pomare <luispomare92@gmail.com>
2023-08-28 00:18:15 -0500

    Use conditional rendering

--- src/sections/Main.svelte
+++ src/sections/Main.svelte
@@ -1,24 +1,20 @@
 <script>
-  let todoOne = {value:1, completed: false}
-  let todoTwo = {value:2, completed: false}
+  let todo = {value:1, completed: false}
 
   function toggle1() {
-    todoOne.completed = !todoOne.completed
+    todo.completed = !todo.completed
   }
-  function toggle2() {
-    todoTwo.completed = !todoTwo.completed
-  }
-  $: console.log(todoOne.value)
-  $: console.log(todoTwo.value)
 </script>
 
 <main>
   <div>
-    <p>{todoOne.value}: {todoOne.completed.toString()}</p>
-    <button type="button" on:click={toggle1}></button>
-  </div>
-  <div>
-    <p>{todoTwo.value}: {todoTwo.completed.toString()}</p>
-    <button type="button" on:click={toggle2}></button>
+    <p>{todo.value}: {todo.completed.toString()}</p>
+    {#if todo.completed}
+      <button type="button" on:click={toggle1}>Mark as undone</button>
+    {/if}
+    {#if !todo.completed}
+      <button type="button" on:click={toggle1}>Mark as done</button>
+    {/if}
+
   </div>
 </main>
