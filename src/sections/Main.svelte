10f067dffeecf067a67a890ca4e3dd92c70da9ab
Luis Pomare <luispomare92@gmail.com>
2023-08-27 23:23:13 -0500

    Add console arbitrary statement

--- src/sections/Main.svelte
+++ src/sections/Main.svelte
@@ -1,31 +1,24 @@
 <script>
-  let todos = [
-    {value:'1',
-      completed: false
-    },
-    {value:'2',
-      completed: false
-    },
-  ]
+  let todoOne = {value:1, completed: false}
+  let todoTwo = {value:2, completed: false}
+
   function toggle1() {
-    todos[0].completed = !todos[0].completed
+    todoOne.completed = !todoOne.completed
   }
   function toggle2() {
-    todos[1].completed = !todos[1].completed
+    todoTwo.completed = !todoTwo.completed
   }
+  $: console.log(todoOne.value)
+  $: console.log(todoTwo.value)
 </script>
-
-<style>
-  /* your styles go here */
-</style>
 
 <main>
   <div>
-    <p>{todos[0].value}: {todos[0].completed.toString()}</p>
+    <p>{todoOne.value}: {todoOne.completed.toString()}</p>
     <button type="button" on:click={toggle1}></button>
   </div>
   <div>
-    <p>{todos[1].value}: {todos[1].completed.toString()}</p>
+    <p>{todoTwo.value}: {todoTwo.completed.toString()}</p>
     <button type="button" on:click={toggle2}></button>
   </div>
 </main>
